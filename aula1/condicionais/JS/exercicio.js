2667f2b9fecf6420e8093ac8c81b960e82e05a99
Daniloasgit <danilo.a.carvalho6@aluno.senai.br>
2024-04-12 10:49:50 -0300

    to doente

---
+++ aula1/condicionais/JS/exercicio.js
@@ -0,0 +1,7 @@
+var vel = 50
+console.log(`a velocidade do seu carro é ${vel} km/h`)
+
+if (vel > 60) {
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+}
+console.log('dirija sempre usando o cinto de segurança');
---
+++ aula1/condicionais/JS/exercicio2.js
@@ -0,0 +1,8 @@
+var pais = 'Angola'
+console.log(`vivendo no ${pais}`)
+if (pais == 'angola') {
+console.log ('você é  angolano') /true
+}
+else { 
+    console.log ('você é estrangeiro') /false
+}
