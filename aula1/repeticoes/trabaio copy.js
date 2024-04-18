6cf6f8b0ecfaecc7a462228a1a07bb19f674e833
Daniloasgit <danilo.a.carvalho6@aluno.senai.br>
2024-04-18 10:25:55 -0300

    contar

---
+++ aula1/repeticoes/trabaio copy.js
@@ -0,0 +1,9 @@
+// console.log (' tem fatia?');
+// console.log (' tem fatia ');
+
+
+console.log ('imicio');
+for (var c =1 ; c <= 10; c++) { 
+    console.log (c);
+}
+console.log ('fim');
---
+++ aula1/repeticoes/trabaio.js
@@ -0,0 +1,9 @@
+// console.log (' tem fatia?');
+// console.log (' tem fatia ');
+
+
+var c = 1
+do {
+    console.log(`passo${c}`)
+    c++ //c = c + 1
+}while (c <= 8 ) 
